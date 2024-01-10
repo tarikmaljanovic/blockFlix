cab6cad5bf378054892d18b62e20623b39eb95ef
Tarik Maljanović <tarikmaljanovic123@gmail.com>
2024-01-10 20:24:19 +0100

    logout

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string","name":"","type":"string"}],"name":"MoviePurchased","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string","name":"","type":"string"}],"name":"Subscribed","type":"event"},{"inputs":[{"internalType":"uint256","name":"movieId","type":"uint256"}],"name":"buyMovie","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"string","name":"movieName","type":"string"},{"internalType":"uint256","name":"price","type":"uint256"}],"name":"createMovie","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getAllMovies","outputs":[{"components":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"string","name":"name","type":"string"},{"internalType":"uint256","name":"price","type":"uint256"}],"internalType":"struct Blockflix.Movie[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getMemberMovies","outputs":[{"internalType":"string[]","name":"","type":"string[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getSubscriptionDate","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getUserType","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"date","type":"string"}],"name":"subscribe","outputs":[],"stateMutability":"payable","type":"function"}]
-const address = "0x8464cA5075937C5F18859e8658bA640037299cd8";
+const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string","name":"","type":"string"}],"name":"MoviePurchased","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string","name":"","type":"string"}],"name":"Subscribed","type":"event"},{"inputs":[{"internalType":"uint256","name":"movieId","type":"uint256"}],"name":"buyMovie","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"string","name":"movieName","type":"string"},{"internalType":"uint256","name":"price","type":"uint256"},{"internalType":"string","name":"img","type":"string"}],"name":"createMovie","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getAllMovies","outputs":[{"components":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"string","name":"name","type":"string"},{"internalType":"uint256","name":"price","type":"uint256"},{"internalType":"string","name":"img","type":"string"}],"internalType":"struct Blockflix.Movie[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getMemberMovies","outputs":[{"internalType":"string[]","name":"","type":"string[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getSubscriptionDate","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getUserType","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"date","type":"string"}],"name":"subscribe","outputs":[],"stateMutability":"payable","type":"function"}]
+const address = "0xcEB86A9402cD47B7458eA3D262698df5A7310B6b";
 
 $(document).ready(function () {
     $("#movies").hide();
@@ -15,6 +15,14 @@ $(document).ready(function () {
     
             var owner1 = '0x8756ce22ab4ea8bb5b0d1e6fa8447cde6b25f355';
             var owner2 = '0x39c342a73a510Bc52E059Bac8b1fD530a793B678';
+
+            if(addresses[0] == owner1) {
+                $('.admin-name').html('Tarik Maljanovic')
+            } else if(addresses[0] == owner2) {
+                $('.admin-name').html('Melisa Geca')
+            } else {
+                $('.admin').hide()
+            }
             
             if(addresses.includes(owner1) == false || addresses.includes(owner2)) {
                 $("#create-movie").hide()
@@ -42,12 +50,16 @@ $(document).ready(function () {
         $('#movieModal').hide();
         $('.alert').hide();
     });
+
+    $('#logout').click(function() {
+        window.location.reload()
+    })
     
     $('#create-button').click(function() {
-        if($('#movie-name').val() == '' || $('#movie-price').val() < 0) {
+        if($('#movie-name').val() == '' || $('#movie-price').val() < 0 || $('#movie-img').val() == '') {
             $('#creation-error').show();
         } else {
-            createMovie($('#movie-name').val(), $('#movie-price').val())
+            createMovie($('#movie-name').val(), $('#movie-price').val(), $('#movie-img').val())
             $('#movieModal').hide();
         }
     })
@@ -84,18 +96,28 @@ async function getMembersMovies() {
 
         if((await isPlusMember) == 'MemberPlus') {
             content += `
-                <div id="${movieId}" class="movie">
-                    <p class="text">${(await movie).name}</p>
-                    <p class="text">${(await movie).price} Wei</p>
-                    ${(await isExpired()) ? '' : '<button type="button" class="btn btn-primary watch-button">Watch Now</button>'}
+                <div id="${(await movie).id}" class="movie">
+                    <div class="img-container">
+                        <img src='${(await movie).img}'/>
+                    </div>
+                    <div class="right">
+                        <p class="text"><strong>${(await movie).name}</strong></p>
+                        <p class="text">${(await movie).price} Wei</p>
+                        ${(await isExpired()) ? '' : '<button type="button" class="btn btn-primary watch-button">Watch Now</button>'}
+                    </div>
                 </div>
             `;
         } else {
             content += `
-                <div id="${movieId}" class="movie">
-                    <p class="text">${(await movie).name}</p>
-                    <p class="text">${(await movie).price} Wei</p>
-                    ${isMemberMovie ? `<button type="button" class="btn btn-primary watch-button">Watch Now</button>` : `<button onclick="buyMovie(${(await movie).id}, ${(await movie).price})" type="button" class="btn btn-success buy-button">Buy</button>`}
+                <div id="${(await movie).id}" class="movie">
+                    <div class="img-container">
+                        <img src='${(await movie).img}'/>
+                    </div>
+                    <div class="right">
+                        <p class="text"><strong>${(await movie).name}</strong></p>
+                        <p class="text">${(await movie).price} Wei</p>
+                        ${isMemberMovie ? `<button type="button" class="btn btn-primary watch-button">Watch Now</button>` : `<button onclick="buyMovie(${(await movie).id}, ${(await movie).price})" type="button" class="btn btn-success buy-button">Buy</button>`}
+                    </div>
                 </div>
             `;
         }
@@ -111,8 +133,6 @@ async function buyMovie(id, price) {
         console.log("Buy Movie Result:", result);
     });
 
-    getMembersMovies()
-
     contract.events.MoviePurchased().on('data', function (event) {
         console.log(event);
         $('#success-msg').html('You have successfully purchased a movie!')
@@ -120,15 +140,18 @@ async function buyMovie(id, price) {
 
         setTimeout(() => {
             $('#success-msg').hide();
+
+            getMembersMovies()
+            
         }, 5000)
     })
 }
 
-async function createMovie(movieName, price) {
+async function createMovie(movieName, price, img) {
     const contract = new web3.eth.Contract(abi, address);
     let addresses = await window.ethereum.request({ method: 'eth_requestAccounts' });
 
-    contract.methods.createMovie(movieName, price).send({ from: addresses[0] }).then(function (result) {	
+    contract.methods.createMovie(movieName, price, img).send({ from: addresses[0] }).then(function (result) {	
         console.log(result);
     })
 
@@ -165,7 +188,6 @@ async function paySubscription(date) {
         console.log("Subscription:", result);
     });
 
-    getMembersMovies()
 
     contract.events.Subscribed().on('data', function (event) {
         console.log(event);
@@ -175,6 +197,9 @@ async function paySubscription(date) {
         setTimeout(() => {
             $('#success-msg').hide();
         }, 5000)
+
+        getMembersMovies()
+
     })
 }
 
